5008b348bd9a2a4fef8fafb6fa952e83e067894a
Listzi <alexisfoot2010@gmail.com>
2024-05-31 16:32:20 +0200

    exam rendu

--- app/controllers/application.controller.mjs
+++ app/controllers/application.controller.mjs
@@ -1,5 +1,16 @@
 // Votre travail doit être effectué principalement dans ce fichier ...
 
+
 export function loadApplicationController(app) {
-    // définir les routes de l'application ici ...
+    views.get('/', (req, res) => {
+        const connexion = req.session
+        res.render('formNote')
+    })
+
+    app.post('/service') //appelle de la donction getDatabase
+    //get note id
+    // get note id edit
+    //get share
+    //post share 
+    // get share id edit
 }
